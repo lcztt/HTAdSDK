54bad34656df81e5076a34cd4cbfed70f80a6af4
vitas <vitas@MacBook-Pro-2.local>
2020-07-26 22:51:35 +0800

    fix bug

--- Example/Pods/Target Support Files/HTAdSDK/HTAdSDK-umbrella.h
+++ Example/Pods/Target Support Files/HTAdSDK/HTAdSDK-umbrella.h
@@ -10,6 +10,36 @@
 #endif
 #endif
 
+#import "BaiduMobAdBaseNativeAdDelegate.h"
+#import "BaiduMobAdBaseNativeAdObject.h"
+#import "BaiduMobAdBaseNativeAdView.h"
+#import "BaiduMobAdCommonConfig.h"
+#import "BaiduMobAdDelegateProtocol.h"
+#import "BaiduMobAdHybridAdManager.h"
+#import "BaiduMobAdInterstitial.h"
+#import "BaiduMobAdInterstitialDelegate.h"
+#import "BaiduMobAdNative.h"
+#import "BaiduMobAdNativeAdDelegate.h"
+#import "BaiduMobAdNativeAdObject.h"
+#import "BaiduMobAdNativeAdView.h"
+#import "BaiduMobAdNativeVideoBaseView.h"
+#import "BaiduMobAdNativeVideoView.h"
+#import "BaiduMobAdNativeVideoViewDelegate.h"
+#import "BaiduMobAdNativeWebView.h"
+#import "BaiduMobAdPreroll.h"
+#import "BaiduMobAdPrerollDelegate.h"
+#import "BaiduMobAdRewardVideo.h"
+#import "BaiduMobAdRewardVideoDelegate.h"
+#import "BaiduMobAdSetting.h"
+#import "BaiduMobAdSmartFeedView.h"
+#import "BaiduMobAdSplash.h"
+#import "BaiduMobAdSplashDelegate.h"
+#import "BaiduMobAdVideoView.h"
+#import "BaiduMobAdView.h"
+#import "BaiduMobCpuInfoManager.h"
+#import "BDAdSDKManager.h"
+#import "HTBUAdSDKManager.h"
+#import "HTGDTAdSDKManager.h"
 
 FOUNDATION_EXPORT double HTAdSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char HTAdSDKVersionString[];
